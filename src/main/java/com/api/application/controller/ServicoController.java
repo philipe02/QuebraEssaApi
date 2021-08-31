5306ced0c4f98c0811bf191162e354836467fb49
Philipe Santos <philipe_02@hotmail.com>
2021-08-31 19:14:33 -0300

    Adicionando cross origin

--- src/main/java/com/api/application/controller/ServicoController.java
+++ src/main/java/com/api/application/controller/ServicoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.api.application.dto.ServicoDTO;
 import com.api.application.model.Servico;
 import com.api.application.service.ServicoService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/servico")
 public class ServicoController {
